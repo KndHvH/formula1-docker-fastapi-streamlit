5f7501d2c3f4a192dc9102be675860da3180f16a
KndHvH <delend89@gmail.com>
2023-08-03 14:46:30 -0300

    feat: backend get tables

--- backend/api.py
+++ backend/api.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
+from routers import f1routes
 from routers import health_check
 from routers import root
-
 
 app = FastAPI()
 
 app.include_router(root.router)
 app.include_router(health_check.router)
+app.include_router(f1routes.router)
